db28da3fa0fddb241c2123cc349a98de58d7ec22
Sergey Ivanov <aureolu@gmail.com>
2024-08-06 00:51:30 +0300

    divan.ru parsing by scrapy&spider

---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,26 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    # start_urls = ["https://divan.ru/category/divany-i-kresla"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse1(self, response):
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                'name': divan.css('div.lsooF span::text').get(),
+                'price': divan.css('div.pY3d2 span::text').get(),
+                'url': divan.css('a').attrib['href']
+            }
+
+    def parse(self, response):
+        lamps = response.css('div._Ud0k')
+        for lamp in lamps:
+            yield {
+                'name': lamp.css('div.lsooF span::text').get(),
+                'price': lamp.css('div.pY3d2 span::text').get(),
+                'url': lamp.css('a').attrib['href']
+            }
